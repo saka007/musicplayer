f8b81eed438fb1ddb71a63b07f323b74522f4d34
Firstname Lastname <saquibrahmani10182@gmail.com>
2021-08-14 17:50:40 +0530

    Updated

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+  {
+    name: "Aaj Bhi",
+    artist: "Vishal Mishra",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Hawa Banke",
+    artist: "Darshan Raval",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Rabba Mehar",
+    artist: "Darshan Raval",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Insane",
+    artist: "AP Dhillon",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Arrogant",
+    artist: "AP Dhillon",
+    img: "music-5",
+    src: "music-5"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
